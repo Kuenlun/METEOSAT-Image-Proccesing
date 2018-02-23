3469be1f3c94249225f5b5282ba291e787e6e9e5
Kuenlun <kuenlun13054@gmail.com>
2018-02-23 13:14:48 +0100

    Adding satellite data and starting^Codifying the python script

--- sat_to_img.py
+++ sat_to_img.py
@@ -1,1 +1,10 @@
 # Converting raw satellite data into images
+
+# Necesary libraries
+from netCDF4 import Dataset
+import os
+
+os.chdir('eumetsat_data')
+file_name = 'W_XX-EUMETSAT-Darmstadt,VIS+IR+IMAGERY,MSG2+SEVIRI_C_EUMG_20070904180010.nc'
+dataset = Dataset(os.getcwd() + '\\' + file_name)
+print(dataset)
