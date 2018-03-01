ed5b03e391fd033cfe1cf739d608217c1bdf8d9c
Kuenlun <kuenlun13054@gmail.com>
2018-03-01 01:05:07 +0100

    Adding more example images

--- example.py
+++ example.py
@@ -10,6 +10,7 @@ file_path = data_path + '\\' + 'W_XX-EUMETSAT-Darmstadt,VIS+IR+IMAGERY,MSG3+SEVI
 # Tell SatImg to read the HDF file
 sat.read_file(file_path)
 
+
 # Making the color array
 color_array = sat.color()
 sat.create_image(color_array, images_path, 'Color.jpeg')
@@ -22,3 +23,17 @@ sat.create_image(colorgeo1_array, images_path, 'ColorGeo.jpeg')
 
 colorgeo2_array = sat.latlon(color_array, way=2)
 sat.create_image(colorgeo2_array, images_path, 'ColorGeo Modo 2.jpeg')
+
+
+# Making the dust array
+dust_array = sat.dust()
+sat.create_image(dust_array, images_path, 'Dust.jpeg')
+
+dustgeo1bilinter_array = sat.latlon(dust_array)
+sat.create_image(dustgeo1bilinter_array, images_path, 'DustGeo Bilinter.jpeg')
+
+dustgeo1_array = sat.latlon(dust_array, interpolation=False)
+sat.create_image(dustgeo1_array, images_path, 'DustGeo.jpeg')
+
+dustgeo2_array = sat.latlon(dust_array, way=2)
+sat.create_image(dustgeo2_array, images_path, 'DustGeo Modo 2.jpeg')
